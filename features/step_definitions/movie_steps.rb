5aaeb9d7967db4b167a6ed96547b9376ae5dbdb9
Nikhil Gupta <saasbook@saasbook.(none)>
2013-02-11 02:06:36 +0530

    cucumber tests

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,10 +2,11 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
+    Movie.create!(movie)
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +15,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
+  assert page.body =~ /(.)*#{Regexp.escape(e1)}(.)*#{Regexp.escape(e2)}/im
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,4 +27,27 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  ratings = rating_list.split(%r{,\s*})
+  ratings.each do |rating|
+    if uncheck
+      steps (%Q(When I uncheck "ratings[#{rating}]"))
+    else
+      steps (%Q(When I check "ratings[#{rating}]"))
+    end
+  end
 end
+
+Then /I should see all of the movies/ do
+  numberOfRows = page.all('table tbody tr').size
+  numberOfMovies = Movie.find(:all).count
+  assert_equal numberOfMovies, numberOfRows
+end
+
+Then /I should see no movies/ do
+  numberOfRows = page.all('table tbody tr').size
+  assert_equal 0, numberOfRows
+end
+Then /the movies should be sorted alphabetically/ do
+  check_order Movie.all(:order => :title)
+end
+
